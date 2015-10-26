3818992c5802bb2aa53ea4c53f28f82d503dbded
Federico Gimenez <fgimenez@coit.es>
2015-10-26 13:00:30 +0100

    runner login closes #9

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -19,10 +19,92 @@
 // Package runner handles the execution entry point
 package runner
 
-import "github.com/fgimenez/snappy-cloud-image/pkg/flags"
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/fgimenez/snappy-cloud-image/pkg/flags"
+	"github.com/fgimenez/snappy-cloud-image/pkg/image"
+)
+
+// Runner is the main type of the package
+type Runner struct {
+	imgDataOrigin image.Pollster
+	imgDataTarget image.PollsterCreator
+	imgDriver     image.Driver
+}
+
+// ErrVersion is the type of the error returned by Exec when the version
+// in SI is greater than or equal the version in cloud
+type ErrVersion struct {
+	siVersion, cloudVersion int
+}
+
+func (e *ErrVersion) Error() string {
+	return fmt.Sprintf("error SI version %d is lower than cloud version %d", e.siVersion, e.cloudVersion)
+}
+
+// ErrActionUnknown is the type of the error returned by Exec when the
+// action given is not recognized
+type ErrActionUnknown struct {
+	action string
+}
+
+func (e *ErrActionUnknown) Error() string {
+	return fmt.Sprintf("error unknown action %s", e.action)
+}
 
 // Exec is the main entry point, it interprets the given options and
 // handles the logic of the utility
-func Exec(options *flags.Options) (err error) {
+func (r *Runner) Exec(options *flags.Options) (err error) {
+	if options.Action == "create" {
+		var siVersion, cloudVersion int
+		siVersion, cloudVersion, err = r.getVersions(options.Release, options.Channel, options.Arch)
+		if err != nil {
+			return
+		}
+		if siVersion <= cloudVersion {
+			return &ErrVersion{siVersion, cloudVersion}
+		}
+		var path string
+		path, err = r.imgDriver.Create(options.Release, options.Channel, options.Arch, siVersion)
+		defer os.Remove(path)
+		if err != nil {
+			return
+		}
+		err = r.imgDataTarget.Create(path, options.Release, options.Channel, options.Arch, siVersion)
+		if err != nil {
+			return
+		}
+	} else {
+		return &ErrActionUnknown{action: options.Action}
+	}
+	return
+}
+
+func (r *Runner) getVersions(release, channel, arch string) (siVersion, cloudVersion int, err error) {
+	var wg sync.WaitGroup
+	var siError, cloudError error
+
+	wg.Add(1)
+	go func() {
+		siVersion, siError = r.imgDataOrigin.GetLatestVersion(release, channel, arch)
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		cloudVersion, cloudError = r.imgDataTarget.GetLatestVersion(release, channel, arch)
+		wg.Done()
+	}()
+
+	wg.Wait()
+	if siError != nil {
+		return 0, 0, siError
+	}
+	if cloudError != nil {
+		return 0, 0, cloudError
+	}
 	return
 }
